6eaf869e30f600da90df86fea7718e4c5d95d0e5
신동욱 <dw@sindong-ug-ui-MacBookAir.local>
2023-04-11 20:23:33 +0900

    1

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sys
 import os
 
 
-UI_PATH = os.path.dirname(os.path.realpath(__file__)) + "\\gui\\main.ui"
-
+UI_PATH = os.path.dirname(os.path.realpath(__file__)) + "/gui/main.ui"
+DB_FORM_PATH = os.path.dirname(os.path.realpath(__file__)) + "/config/dbform.csv"
 
 class MainWindow(QMainWindow, uic.loadUiType(UI_PATH)[0]):
     def __init__(self):
@@ -19,7 +19,7 @@ class MainWindow(QMainWindow, uic.loadUiType(UI_PATH)[0]):
         self.push_button_open_table_form.clicked.connect(self.open_table_form)
 
     def create_table_form_and_open(self):
-        pass
+        
 
     def insert_table_from_table_form(self):
         pass
