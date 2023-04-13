60971cdecb151f4b6c73ff243374d7d87a941b2d
aojiswag <idongook3@gmail.com>
2023-04-13 17:56:22 +0900

    1

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic, QtGui
 import sys
 import os
+import shutil
 
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+UI_PATH = BASE_PATH + "/gui/main.ui"
+DEFAULT_DB_FORM_PATH = BASE_PATH + "/config/dbform.csv"
 
-UI_PATH = os.path.dirname(os.path.realpath(__file__)) + "/gui/main.ui"
-DB_FORM_PATH = os.path.dirname(os.path.realpath(__file__)) + "/config/dbform.csv"
 
 class MainWindow(QMainWindow, uic.loadUiType(UI_PATH)[0]):
     def __init__(self):
@@ -19,13 +21,19 @@ class MainWindow(QMainWindow, uic.loadUiType(UI_PATH)[0]):
         self.push_button_open_table_form.clicked.connect(self.open_table_form)
 
     def create_table_form_and_open(self):
-        
+        form_file = QFileDialog.getSaveFileName(self, 'Add form', BASE_PATH,
+                                                "Comma Separated Values (*.csv)")
+        shutil.copy(DEFAULT_DB_FORM_PATH, form_file[0])
+        self.line_edit_table_form_dir.setText(form_file[0])
 
     def insert_table_from_table_form(self):
         pass
 
     def open_table_form(self):
-        pass
+        form_file = QFileDialog.getOpenFileName(self, 'Add form', BASE_PATH,
+                                                "Comma Separated Values (*.csv)")
+        self.line_edit_table_form_dir.setText(form_file[0])
+
 
     def resizeEvent(self, a0: QtGui.QResizeEvent) -> None:
         # update font size when resized (dynamic)
@@ -35,9 +43,8 @@ class MainWindow(QMainWindow, uic.loadUiType(UI_PATH)[0]):
             widget = resize_parent.itemAt(i).widget()
             if widget is not None:
                 font = widget.font()
-                font.setPointSize(int(widget.geometry().height()/2)-2)
+                font.setPointSize(int(widget.geometry().height() / 2) - 2)
                 widget.setFont(font)
-
 
 
 if __name__ == "__main__":
