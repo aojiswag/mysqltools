d8cb8990bea98bdc0640cf951536fd8afce99066
aojiswag <idongook3@gmail.com>
2023-04-16 23:41:37 +0900

    col, datatype, datalen create table implementation

--- main.py
+++ main.py
@@ -6,10 +6,20 @@ import shutil
 import numpy
 import pymysql
 import csv
+import win32api
+import threading
 
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 UI_PATH = BASE_PATH + "/gui/main.ui"
 DEFAULT_DB_FORM_PATH = BASE_PATH + "/config/dbform.csv"
+
+
+def msg_box(text: str, title: str, utype: int):
+    ok = win32api.MessageBox(0, text, title, utype)
+
+
+def open_with_window(path: str):
+    os.system(path)
 
 
 class MainWindow(QMainWindow, uic.loadUiType(UI_PATH)[0]):
@@ -20,33 +30,59 @@ class MainWindow(QMainWindow, uic.loadUiType(UI_PATH)[0]):
 
     def init_ui(self):
         self.push_button_create_table_form_and_open.clicked.connect(self.create_table_form_and_open)
-        self.push_button_insert_table_from_table_form.clicked.connect(self.insert_table_from_table_form)
+        self.push_button_create_table_from_table_form.clicked.connect(self.create_table_from_table_form)
         self.push_button_open_table_form.clicked.connect(self.open_table_form)
 
     def create_table_form_and_open(self):
-        form_file = QFileDialog.getSaveFileName(self, 'Add form', BASE_PATH,
-                                                "Comma Separated Values (*.csv)")
-        shutil.copy(DEFAULT_DB_FORM_PATH, form_file[0])
-        self.line_edit_table_form_dir.setText(form_file[0])
+        try:
+            form_file = QFileDialog.getSaveFileName(self, 'Add form', BASE_PATH,
+                                                    "Comma Separated Values (*.csv)")
+            shutil.copy(DEFAULT_DB_FORM_PATH, form_file[0])
+            self.line_edit_table_form_dir.setText(form_file[0])
 
-    def insert_table_from_table_form(self):
-        connection = pymysql.connect(host="127.0.0.1", user="root", password=self.line_edit_password.text(),
-                                     db=self.line_edit_db_name.text(), charset='utf8')
+            file_edit_thread = threading.Thread(target=open_with_window, args=(form_file[0],))
+            file_edit_thread.start()
+
+        except FileNotFoundError:
+            return
+
+    def create_table_from_table_form(self):
+        try:
+            connection = pymysql.connect(host="127.0.0.1", user="root", password=self.line_edit_password.text(),
+                                         db=self.line_edit_db_name.text(), charset='utf8')
+        except pymysql.err.OperationalError:
+            msg_box("Dbname or password error", "error", 0)
+
+            return
+
         db = connection.cursor()
-
         with open(self.line_edit_table_form_dir.text(), newline='') as table_form_file:
             reader = csv.reader(table_form_file)
             data = list(reader)
 
-        print(data)
+        sql = "CREATE TABLE " + self.line_edit_table_name.text() + "("
+
+        for i in range(1, len(data), 1):
+            if data[i][0] != "" and data[i][1] != "" and data[i][2] != "":
+                if i == len(data) - 1:
+                    sql = sql + "{0} {1}({2}))".format(data[i][0], data[i][1], data[i][2])
+                else:
+                    sql = sql + "{0} {1}({2}),".format(data[i][0], data[i][1], data[i][2])
+
+        print(sql)
+        db.execute(sql)
+
+        connection.close()
 
     def open_table_form(self):
-        form_file = QFileDialog.getOpenFileName(self, 'Add form', BASE_PATH,
-                                                "Comma Separated Values (*.csv)")
-        self.line_edit_table_form_dir.setText(form_file[0])
+        try:
+            form_file = QFileDialog.getOpenFileName(self, 'Add form', BASE_PATH,
+                                                    "Comma Separated Values (*.csv)")
+            self.line_edit_table_form_dir.setText(form_file[0])
+        except FileNotFoundError:
+            return
 
     def resizeEvent(self, a0: QtGui.QResizeEvent) -> None:
-        print("evented")
         # update font size when resized (dynamic)
         resize_parent = self.findChildren(QGridLayout, name="grid_layout_insert_table_tool")[0]
         for i in range(resize_parent.count()):
